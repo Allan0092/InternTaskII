9b0ebc34719d87d65167cec95149d323df69b91e
Allan Gautam <allan.gtm92@gmail.com>
2026-01-28 11:52:16 +0545

    added axios to fetch from server

--- frontend/src/Blog.jsx
+++ frontend/src/Blog.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { sendAxiosRequest } from "./api";
 
 const Blog = () => {
   const { slug } = useParams();
-  return <>Blog page according to Slug: {slug}.</>;
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      const res = await sendAxiosRequest({ method: "get", url: `/${slug}` });
+      console.log(res.data);
+      setBlog(res.data);
+    };
+    fetchBlog();
+  }, []);
+  return (
+    <div className="flex">
+      <div className="w-full h-full">
+        <h1>{blog.title}.</h1>
+      </div>
+      <div className="flex">
+        <p>{blog.content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,17 +4,50 @@
 
 import axios from "axios";
 
-// const URL = import.meta.env.VITE_SERVER_URL;
+const URL = import.meta.env.VITE_SERVER_URL;
+
+const sendAxiosRequest = async ({
+  method,
+  url = URL,
+  body = {},
+  query = {},
+  header = {},
+}) => {
+  const api = axios.create({
+    baseURL: `${URL}`,
+    headers: header,
+    url: url,
+    method: method,
+  });
+  try {
+    const response = await axios({
+      method: method.toLowerCase(),
+      baseURL: URL,
+      url: url,
+      params: query,
+      data: body,
+      headers: {
+        "Content-Type": "application/json",
+        ...header,
+      },
+    });
+    return response.data;
+  } catch (e) {
+    console.error("error in sendAxiosRequest: ", e);
+  }
+};
 
 const getAllBlog = async () => {
-  const req = await axios.get("http://localhost:3001/blog");
-  if (req.status === axios.HttpStatusCode.Ok) {
-    console.log(`Get All Blogs: ${req}`);
+  //   const req = await axios.get("http://localhost:3001/blog");
+  //   if (req.status === axios.HttpStatusCode.Ok) {
+  //     console.log(`Get All Blogs: ${req}`);
 
-    return req.data?.message;
-  } else {
-    console.error(req);
-  }
+  //     return req.data?.message;
+  //   } else {
+  //     console.error(req);
+  //   }
+  const blogs = await sendAxiosRequest((method = "get"));
+  return blogs;
 };
 const createBlog = async (blog) => {
   const request = await axios.post("localhost:3001/blog", blog);
@@ -31,4 +64,4 @@ const getBySlug = async (slug) => {
   }
 };
 
-export { createBlog, getAllBlog, getBySlug };
+export { createBlog, getAllBlog, getBySlug, sendAxiosRequest };
